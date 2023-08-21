f1b7415dead754fad41c0a85cc0e9980340c9b6a
nazmul906 <nazmulhb96@gmail.com>
2023-08-21 15:37:47 +0600

    login page is added

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const handleClick = () => {};
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="flex justify-center items-center h-screen  bg-slate-400">
+        <div className="w-full max-w-md ">
+          <form
+            className="shadow-md rounded px-8 py-14 bg-slate-300"
+            onSubmit={handleClick}
+          >
+            <div className="mb-4">
+              <h3 className="text-center">Welcome to Login!</h3>
+              <label className="block text-gray-700 text-sm font-bold mb-2">
+                Email
+              </label>
+              <input
+                className="w-full px-3 py-2 border rounded-md"
+                name="email"
+                type="text"
+                placeholder="Enter your email"
+              />
+            </div>
+            <div className="mb-4">
+              <label className="block text-gray-700 text-sm font-bold mb-2">
+                Password
+              </label>
+              <input
+                className="w-full px-3 py-2 border rounded-md "
+                type="password"
+                name="password"
+                placeholder="Enter your password"
+              />
+            </div>
+
+            <input className="btn btn-primary" type="submit" value="submit" />
+          </form>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
