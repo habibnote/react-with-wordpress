96517e6bb8411b0f8547512769c080518b3ecd92
nazmul906 <nazmulhb96@gmail.com>
2023-08-22 08:44:29 +0600

    react routing is applied to navigate

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,7 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const handleClick = () => {};
-  return (
-    <>
-      <div className="flex justify-center items-center h-screen  bg-slate-400">
-        <div className="w-full max-w-md ">
-          <form
-            className="shadow-md rounded px-8 py-14 bg-slate-300"
-            onSubmit={handleClick}
-          >
-            <div className="mb-4">
-              <h3 className="text-center">Welcome to Login!</h3>
-              <label className="block text-gray-700 text-sm font-bold mb-2">
-                Email
-              </label>
-              <input
-                className="w-full px-3 py-2 border rounded-md"
-                name="email"
-                type="text"
-                placeholder="Enter your email"
-              />
-            </div>
-            <div className="mb-4">
-              <label className="block text-gray-700 text-sm font-bold mb-2">
-                Password
-              </label>
-              <input
-                className="w-full px-3 py-2 border rounded-md "
-                type="password"
-                name="password"
-                placeholder="Enter your password"
-              />
-            </div>
-
-            <input className="btn btn-primary" type="submit" value="submit" />
-          </form>
-        </div>
-      </div>
-    </>
-  );
+  return <div></div>;
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,29 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Main from "./component/Main/Main.jsx";
+import Home from "./component/Home/Home.jsx";
+import Login from "./component/Login/Login.jsx";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "/log",
+        element: <Login></Login>,
+      },
+    ],
+  },
+]);
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <RouterProvider router={router}> </RouterProvider>
   </React.StrictMode>
 );
