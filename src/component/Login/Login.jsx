e37149b285bc2b6b5b2d498e5b8d2b9b3084572d
Mehraz Morshed <mehrazmorshed@gmail.com>
2023-08-23 10:17:50 +0600

    Update Login.jsx
    WordPress Coding Standards

--- src/component/Login/Login.jsx
+++ src/component/Login/Login.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import axios from "axios";
-
 import { useEffect } from "react";
+
 function Login() {
   const [error, setError] = useState([]);
   const [nonce, setNonce] = useState("");
@@ -9,14 +9,13 @@ function Login() {
   useEffect(() => {
     fetchNonce();
   }, []);
-
+  
   const fetchNonce = async () => {
     try {
       const response = await fetch(
         "http://localhost/wordpress/wp-json/custom-auth-api/v1/nonce"
       );
       const data = await response.json();
-
       setNonce(data.nonce);
       setIsLoading(false);
     } catch (error) {
